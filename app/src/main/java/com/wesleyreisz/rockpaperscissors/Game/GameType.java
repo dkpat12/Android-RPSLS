5d273a71c153afc13dd968cb8ef68b7ab2ac5afa
Dillion Patrick <dkpat12@gmail.com>
2015-09-24 14:34:08 -0400

    Completed App
    I didn't make any intermediate commits. Oops.

--- app/src/main/java/com/wesleyreisz/rockpaperscissors/Game/GameType.java
+++ app/src/main/java/com/wesleyreisz/rockpaperscissors/Game/GameType.java
@@ -4,5 +4,5 @@ package com.wesleyreisz.rockpaperscissors.Game;
  * Created by wesleyreisz on 9/13/15.
  */
 public interface GameType {
-    public String eval(Integer opponentChoice);
+    String eval(Integer opponentChoice);
 }
